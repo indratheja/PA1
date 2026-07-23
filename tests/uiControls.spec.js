9748ca62e025ddc89c3725bbe0af6defa39ba90c
indratheja <indratheja425@gmail.com>
2026-07-23 13:06:35 +0530

    cr

--- tests/uiControls.spec.js
+++ tests/uiControls.spec.js
@@ -76,7 +76,7 @@ test("datetime picker", async ({ page }) => {
 
 });
 
-test.only('demo flight booking', async ({ page }) => {
+test('demo flight booking', async ({ page }) => {
 
     await page.goto("https://www.dummyticket.com/dummy-ticket-for-visa-application/");
     const date = "22";
